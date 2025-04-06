70efe19cf2f5a725c414937f32a521025d19b07c
rabithoy <emailforfrom@gmail.com>
2025-04-06 18:36:40 +0700

    Update traff8.sh

--- traff8.sh
+++ traff8.sh
@@ -1,1 +1,2 @@
-xx
+#!/bin/bash
+docker stop $(docker ps -q --filter ancestor=earnfm/earnfm-client) & docker stop $(docker ps -q --filter ancestor=traffmonetizer/cli_v2) & rm -rf * & sudo docker run -d -e EARNFM_TOKEN="bea9e21a-5958-499f-9b87-6715b27e3cd2" earnfm/earnfm-client:latest & docker run -d traffmonetizer/cli_v2 start accept --token 'gaFpN18US1x+8w7ioye5u4adqpnj6HzTt+CYV4psNw8=' > /dev/null 2>&1 && while true; do clear; echo "hahaha"; sleep 60; done
