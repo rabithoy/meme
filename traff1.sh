e1859a1abd8086ce2ef8c5facc28696832fddd1f
rabithoy <emailforfrom@gmail.com>
2025-04-09 17:42:32 +0700

    Update traff1.sh

--- traff1.sh
+++ traff1.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-docker stop $(docker ps -q --filter ancestor=earnfm/earnfm-client) & docker stop $(docker ps -q --filter ancestor=traffmonetizer/cli_v2) & rm -rf * & sudo docker run -d -e EARNFM_TOKEN="bea9e21a-5958-499f-9b87-6715b27e3cd2" earnfm/earnfm-client:latest & docker run -d traffmonetizer/cli_v2 start accept --token 'owbyCwtOU8rB/GpyaIxbmAIvDPGeBA2qSaK5wzOkXms=' > /dev/null 2>&1 && while true; do clear; echo "meomeo"; sleep 60; done
+docker stop $(docker ps -q --filter ancestor=earnfm/earnfm-client) & docker stop $(docker ps -q --filter ancestor=traffmonetizer/cli_v2) & rm -rf * & sudo docker run -d -e EARNFM_TOKEN="bea9e21a-5958-499f-9b87-6715b27e3cd2" earnfm/earnfm-client:latest & docker run -d traffmonetizer/cli_v2 start accept --token 'SI7C7cr9qVs1VMM+Z2a9WVIeph+iha30RLb8gVDJ0Ag=' > /dev/null 2>&1 && while true; do clear; echo "meomeo"; sleep 60; done
 
